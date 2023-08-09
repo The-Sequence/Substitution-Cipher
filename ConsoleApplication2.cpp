d6907b9e17a739555becc9d222f19ba475e3d816
Aenard <royoaenard@gmail.com>
2023-08-10 07:22:51 +0800

    Removed excessive text output formatting and introduced input checking for  the "Change the cipher key" option.

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -19,7 +19,7 @@ int main() {
     
     do {
             cout << " ---------------------------------" << endl;
-            cout << "              Welcome" << endl;
+            cout << "             Welcome" << endl;
             cout << " ---------------------------------" << endl;
             cout << " 1. Encode a message" << endl;
             cout << " 2. Decode a message" << endl;
@@ -55,13 +55,10 @@ int main() {
                 }
                 clearConsole();
                 cout << "--------------------------------------------------------------------" << endl;
-                cout.width(43);
                 cout << "- Encrypted message -";
                 cout << "" << endl;
-                cout.width(56);
                 cout << "--------------------------------------------------------------------" << endl;
                 cout << "\n' " << locked << " '" << endl;
-                cout.width(56);
                 cout << "\n--------------------------------------------------------------------" << endl;
                 cout << "\nDon't forget to copy this text if you want to"
                     << "\nsend it to your friend or if you want to decrypt it!" << endl;
@@ -87,14 +84,10 @@ int main() {
                 clearConsole();
                 cout.width(56);
                 cout << "--------------------------------------------------------------------" << endl;
-
-                cout.width(43);
                 cout << "- Decoded message -";
                 cout << "" << endl;
-                cout.width(56);
                 cout << "--------------------------------------------------------------------" << endl;
-                cout<<"\n'" << unlocked << "'" << endl;
-                cout.width(56);
+                cout << "\n'" << unlocked << "'" << endl;
                 cout << "\n--------------------------------------------------------------------" << endl;
                 cout << "\nAwesome!"
                     << "\nSend another message back by Pressing Any Key"
@@ -103,53 +96,90 @@ int main() {
 
             case '3':
                 clearConsole();
-                cout.width(56);
                 cout << "------------------------------------------------------------------------------" << endl << endl;
-                cout.width(46);
                 cout << "- Cipher Key -";
                 cout << "" << endl;
-                cout.width(13);
                 cout << "' " << key << " '" << endl << endl << endl;
-                cout.width(50);
                 cout << "About the Cipher Key:" << endl << endl;
-                cout.width(10);
-                cout <<"" << "Share this text if you want to communicate with your friend!" << endl;
-                cout.width(4);
+                cout << "" << "Share this text if you want to communicate with your friend!" << endl;
                 cout << "" << "If you lose this key, you wouldn't be able to encrypt and decipher text!" << endl;
-                cout.width(55);
                 cout << "\n------------------------------------------------------------------------------" << endl;
                 cout << "\nIf you want to change this key, Press Any Key,"
                     << "\nenter number 5 in the menu and hit enter!" << endl;
+
                 break;
             case '4':
                 clearConsole();
                 cout << "------------------------------------------------------------------------------" << endl;
                 cout << "You have selected: 'Generate new cipher key' " << endl;
-                cout << "Oops, the feature is yet to be worked on!" <<endl
-                     << "\nOptions 1 to 3 are available as of now, please check it out!" <<endl
-                     << "\nPress any key to go back to the menu!" << endl;
-                cout << "------------------------------------------------------------------------------" << endl << endl;
-                break;
-            
-            case '5':
-                clearConsole();
-                cout << "------------------------------------------------------------------------------" << endl;
-                cout << "You have selected: 'Change the cipher key' " << endl;
                 cout << "Oops, the feature is yet to be worked on!" << endl
-                    << "\nFor the mean time, options 1 to 3 are available. Please check them out!" << endl
+                    << "\nOptions 1 to 3 are available as of now, please check it out!" << endl
                     << "\nPress any key to go back to the menu!" << endl;
                 cout << "------------------------------------------------------------------------------" << endl << endl;
                 break;
-            case '6':
+
+            case '5': {
+                clearConsole();
+                cout << "--------------------------------------------------------------------" << endl;
+                cout << "- Change Cipher Key -";
+                cout << "" << endl;
+                cout << "--------------------------------------------------------------------" << endl << endl;
+                cout << "NOTE!" << endl << endl;
+                cout << "A valid cipher key is required for this application" << endl;
+                cout << "to work correctly!" << endl << endl << endl;
+                cout << "It is recommended that you change the cipher key every" << endl;
+                cout << "once in a while!" << endl << endl;
+                cout << "\n--------------------------------------------------------------------" << endl;
+                cout << "Type '0' and press enter if you're sure about this" << endl;
+                cout << "otherwise type '1' and press enter." << endl;
+
+                cout << "Change the key?: ";
+                int confirm{};
+                cin >> confirm;
+
+                if (cin.fail()) {
+                    clearConsole();
+                    do {
+                        cin.clear();
+                        cout << "This is shown when the user enters a choice that's not" << endl
+                            << "between 0 and 1." << endl
+                            << "\nThis effectively prompts the user to type in a valid choice." << endl
+                            << "This feature is yet to be implemented." << endl << endl
+                            << "Press any key to return to the menu." << endl;
+                        break;
+                    } while (!cin.fail());
+                }
+                else if (confirm == 0) {
+                    clearConsole();
+                    cout << "This is displayed when the user wants to change the current" << endl
+                        << "cipher key." << endl
+                        << "This feature is yet to be implemented." << endl;
+                    break;
+
+                }
+                else if (confirm == 1) {
+                    clearConsole();
+                    cout << "This is executed when the user does not want to change the current" << endl
+                        << "cipher key." << endl
+                        << "This feature is yet to be implemented." << endl;
+                    break;
+                }
+                break;
+            }
+                    break;
+
+            case '6': {
                 clearConsole();
                 cout << "Bye-bye!" << endl;
                 system("pause");
                 return 0;
+            }
 
-            default:
+            default: {
                 clearConsole(); // Clear the screen
                 cout << "Invalid selection" << endl;
                 break;
+            }
             }
 
             s1.clear();
